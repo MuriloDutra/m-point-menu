9df6909751f88a3f966759b343024599e8324ae2
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2025-06-17 18:27:56 -0300

    Refactor menu structure and styles: Updated HTML to use a section for the menu, added new CSS for menu layout, and restructured JavaScript to support dynamic menu rendering with categories and items.

--- index.js
+++ index.js
@@ -2,65 +2,22 @@ import menu from './menu.js';
 
 const menuContainer = document.getElementById('menu');
 
-console.log(menu);
-
 if (menuContainer) {
     menuContainer.innerHTML = `
-        <h3>Salgados</h3>
-        <ul class="menu-container">
-            <li>
-                <ul class="items">
-                    ${menu.salgados.map(({name, photo, price}) => `
-                        <li class="item">
-                            <img src="${photo}" alt="${name}" class="item-image">
-                            <p>${name}</p>
-                            <p>${price}</p>
-                        </li>
-                    `).join('')}
-                </ul>
-            </li>
-        </ul>
-        <h3>Bebidas</h3>
-        <ul class="menu-container">
-            <li>
-                <ul class="items">
-                    ${menu.bebidas.map(({name, photo, price}) => `
-                        <li class="item">
-                            <img src="${photo}" alt="${name}" class="item-image">
-                            <p>${name}</p>
-                            <p>${price}</p>
-                        </li>
-                    `).join('')}
-                </ul>
-            </li>
-        </ul>
-        <h3>Doces</h3>
-        <ul class="menu-container">
-            <li>
-                <ul class="items">
-                    ${menu.doces.map(({name, photo, price}) => `
-                        <li class="item">
-                            <img src="${photo}" alt="${name}" class="item-image">
-                            <p>${name}</p>
-                            <p>${price}</p>
-                        </li>
-                    `).join('')}
-                </ul>
-            </li>
-        </ul>
-        <h3>Lanches</h3>
-        <ul class="menu-container">
-            <li>
-                <ul class="items">
-                    ${menu.lanches.map(({name, photo, price}) => `
-                        <li class="item">
-                            <img src="${photo}" alt="${name}" class="item-image">
-                            <p>${name}</p>
-                            <p>${price}</p>
-                        </li>
-                    `).join('')}
-                </ul>
-            </li>
-        </ul>
+        ${menu.map(({items, label}) => {
+            return `
+                <div>
+                    ${label?.length > 0 ? `<h2>${label}</h2>` : ''}
+                    <ul class="items">
+                        ${items.map(({name, price, description}) => `
+                            <li>
+                                <p>${name} R$${price}</p>
+                                ${description?.length > 0 ? `<p>${description}</p>` : ''}
+                            </li>
+                        `).join('')}
+                    </ul>
+                </div>
+            `
+        }).join('')}
     `
 }
--- menu.js
+++ menu.js
@@ -1,122 +1,252 @@
-const menu = {
-  salgados: [
-    {
-      name: 'Coxinha',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 5.99
-    },
-    {
-      name: 'Kibe',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 6.50
-    },
-    {
-      name: 'Risole',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 4.99
-    },
-    {
-      name: 'Pastel',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 4.99
-    },
-    {
-      name: 'Coxinha',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 5.99
-    },
-    {
-      name: 'Kibe',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 6.50
-    },
-    {
-      name: 'Risole',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 4.99
-    },
-    {
-      name: 'Coxinha',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 5.99
-    },
-    {
-      name: 'Kibe',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 6.50
-    },
-    {
-      name: 'Risole',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 4.99
-    },
-    {
-      name: 'Coxinha',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 5.99
-    },
-    {
-      name: 'Kibe',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 6.50
-    },
-    {
-      name: 'Risole',
-      photo: 'https://i.panelinha.com.br/i1/bk-6462-coxinha-de-galinha.webp',
-      price: 4.99
-    },
-  ],
-  bebidas: [
-    {
-      name: 'Coca-Cola',
-      photo: 'https://i.panelinha.com.br/i1/bk-5025-suco-de-manga.webp',
-      price: 4.50
-    },
-    {
-      name: 'Pepsi',
-      photo: 'https://i.panelinha.com.br/i1/bk-5025-suco-de-manga.webp',
-      price: 4.50
-    },
-    {
-      name: 'Suco de Laranja',
-      photo: 'https://i.panelinha.com.br/i1/bk-5025-suco-de-manga.webp',
-      price: 6.00
-    }
-  ],
-  doces: [
-    {
-      name: 'Brigadeiro',
-      photo: 'https://i.panelinha.com.br/i1/228-q-2859-brigadeiro.webp',
-      price: 3.50
-    },
-    {
-      name: 'Beijinho',
-      photo: 'https://i.panelinha.com.br/i1/228-q-2859-brigadeiro.webp',
-      price: 3.50
-    },
-    {
-      name: 'Quindim',
-      photo: 'https://i.panelinha.com.br/i1/228-q-2859-brigadeiro.webp',
-      price: 4.00
-    }
-  ],
-  lanches: [
-    {
-      name: 'X-Burger',
-      photo: 'https://i.panelinha.com.br/i1/bk-1181-hamburguer-de-prato.webp',
-      price: 12.99
-    },
-    {
-      name: 'X-Salada',
-      photo: 'https://i.panelinha.com.br/i1/bk-1181-hamburguer-de-prato.webp',
-      price: 14.99
-    },
-    {
-      name: 'X-Bacon',
-      photo: 'https://i.panelinha.com.br/i1/bk-1181-hamburguer-de-prato.webp',
-      price: 15.99
-    }
-  ]
-};
+const sandwiches = [
+  {
+    name: 'Natural de frango',
+    price: 12.00,
+    description: 'Pão de forma, patê de frango, alface e tomate'
+  },
+  {
+    name: 'Bauru',
+    price: 12.00,
+    description: 'Pão francês, presunto, queijo, tomate e orégano'
+  },
+  {
+    name: 'Misto Quente',
+    price: 12.00,
+    description: 'Pão francês, presunto e queijo'
+  },
+  {
+    name: 'Misto Quente c/ ovo',
+    price: 14.00,
+    description: 'Pão francês, presunto, queijo, ovo e orégano'
+  },
+  {
+    name: 'Misto especial',
+    price: 16.00,
+    description: 'Pão francês, presunto, queijo, ovo, orégano, alface e tomate'
+  },
+  {
+    name: 'Pão c/ ovo',
+    price: 11.00,
+    description: 'Pão francês, 2 ovos'
+  },
+  {
+    name: 'Toscana',
+    price: 20.00,
+    description: 'Pão francês, linguiça toscana, queijo, alface e tomate'
+  },
+  {
+    name: 'X-Salada',
+    price: 20.00,
+    description: 'Pão de hamburguer, hamburguer, presunto, queijo, alface e tomate'
+  },
+  {
+    name: 'X-EGG',
+    price: 22.00,
+    description: 'Pão de hamburguer, hamburguer, ovo, presunto, queijo, alface e tomate'
+  },
+  {
+    name: 'X-Picanha',
+    price: 28.00,
+    description: 'Pão de hamburguer, hamburguer de picanha, ovo, presunto, queijo, alface e tomate'
+  },
+  {
+    name: 'X-Linguiça',
+    price: 26.00,
+    description: 'Pão de hamburguer, hamburguer de linguiça toscana, queijo, alface e tomate'
+  },
+  {
+    name: 'Franguinho',
+    price: 20.00,
+    description: 'Pão francês, Filé de frango, queijo, alface e tomate'
+  },
+  {
+    name: 'Churrasquinho',
+    price: 20.00,
+    description: 'Pão francês, Filé bovino, queijo, alface e tomate'
+  },
+  {
+    name: 'Pão na chapa',
+    price: 4.00,
+    description: 'Pão francês c/ manteiga'
+  },
+  {
+    name: 'Pão c/ manteiga',
+    price: 3.50,
+    description: 'Pão francês c/ manteiga'
+  }
+]
+
+const salads = [
+  {
+    name: 'Salada M.Point',
+    price: 21.99,
+    description: 'Alface, tomate, cenoura e carne (filé de frango ou bife carne)'
+  }
+]
+
+const hotDrinks = [
+  {
+    name: 'Café',
+    price: 4.00
+  },
+  {
+    name: 'Pingado',
+    price: 5.00
+  },
+  {
+    name: 'Capuccino',
+    price: 6.00
+  },
+  {
+    name: 'Achocolatado',
+    price: 6.00
+  }
+]
+
+const coldDrinks = [
+  {
+    name: 'Água s/ gás',
+    price: 3.50
+  },
+  {
+    name: 'Água c/ gás',
+    price: 4.50
+  },
+  {
+    name: 'Guaraviton',
+    price: 5.50
+  },
+  {
+    name: 'Achocolatado',
+    price: 6.00
+  },
+  {
+    name: 'Refrigerante caçulinha',
+    price: 4.00
+  },
+  {
+    name: 'Refrigerante KS',
+    price: 5.00
+  },
+  {
+    name: 'Refrigerante lata',
+    price: 7.00
+  },
+  {
+    name: 'Refrigerante 600ml',
+    price: 9.00
+  },
+  {
+    name: 'Suco Del Valle lata',
+    price: 8.00
+  },
+  {
+    name: 'Suco natural de laranja',
+    price: 10.00
+  },
+  {
+    name: 'Suco de polpa',
+    price: 10.00,
+    description: 'Sabores: Abacaxi, Abacaxi c/ hortelã, Maracujá, Morango'
+  },
+  {
+    name: 'Energético Monster',
+    price: 13.00
+  },
+  {
+    name: 'H2O',
+    price: 8.00
+  },
+  {
+    name: 'Refrigerante 1 litro',
+    price: 12.00
+  },
+  {
+    name: 'Refrigerante 1,5 litros',
+    price: 14.00
+  },
+  {
+    name: 'Refrigerante 2 litros',
+    price: 16.00
+  }
+]
+
+const desserts = [
+  {
+    name: 'Pavê',
+    price: 14.00
+  },
+  {
+    name: 'Bolo de pote',
+    price: 14.00,
+    description: 'Vários sabores'
+  },
+  {
+    name: 'Pudim',
+    price: 8.00
+  },
+  {
+    name: 'Torta doce',
+    price: 10.00
+  },
+  {
+    name: 'Açaí',
+    price: 14.00,
+    description: 'Vários sabores'
+  },
+  {
+    name: 'Sorvete',
+    price: 8.00,
+    description: 'Vários sabores'
+  }
+]
+
+const menu = [
+  {
+    label: 'Salgados',
+    items: [
+      {
+        name: 'Salgados assados',
+        price: 9.00
+      },
+      {
+        name: 'Salgados fritos',
+        price: 9.00
+      },
+      {
+        name: 'Torta mini',
+        price: 12.00
+      },
+      {
+        name: 'Torta pedaço',
+        price: 14.00
+      },
+      {
+        name: 'Croassant',
+        price: 12.00
+      }
+    ]
+  },
+  {
+    label: 'Lanches',
+    items: sandwiches
+  },
+  {
+    label: 'Saladas',
+    items: salads
+  },
+  {
+    label: 'Bebidas quentes',
+    items: hotDrinks
+  },
+  {
+    label: 'Bebidas geladas',
+    items: coldDrinks
+  },
+  {
+    label: 'Sobremesas',
+    items: desserts
+  }
+]
 
 export default menu;
